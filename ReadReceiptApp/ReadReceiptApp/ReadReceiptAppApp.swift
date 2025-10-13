0f528576c33a3d6712db2587695368ac5c71ca2e
Alex Ramirez <222163279+AlexSchmalex97@users.noreply.github.com>
2025-10-13 16:57:36 -0400

    Initial Xcode project setup with ReadReceiptApp

---
+++ ReadReceiptApp/ReadReceiptApp/ReadReceiptAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadReceiptAppApp.swift
+//  ReadReceiptApp
+//
+//  Created by Alexandra Ramirez on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ReadReceiptAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
